8d1f680de1e58fe0b8f3b5d1e5f3d0d5b52ed8a7
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2022-03-24 16:28:12 +0530

    basic app setup and eggContainer.js make

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EggContainer from './component/EggContainer';
+
+function App() {
+  return (
+    <div className="App">
+     <EggContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-demo/src/component/EggContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EggContainer = () => {
+  return (
+      <div>
+          <h2>Number of Eggs</h2>
+          <button>Buy Egg</button>
+    </div>
+  )
+}
+
+export default EggContainer
